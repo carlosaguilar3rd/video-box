e7f049f1d938eee651db3773170ceed97c365671
unknown <carlosaguilar3rd@gmail.com>
2023-09-23 01:45:45 +0900

    initial commit

---
+++ src/app/comments/comment-form/command-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-command-form',
+  templateUrl: './command-form.component.html',
+  styleUrls: ['./command-form.component.css']
+})
+export class CommandFormComponent {
+
+}
